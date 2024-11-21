bc30770fb3d88ed41f26833798d541d77116b5b3
mm249 <mm249@email.sc.edu>
2024-11-21 14:28:33 -0500

    add whale image

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -1,24 +1,40 @@
 package com.controllers;
 
-import java.io.IOException;
-import com.language.App;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.event.ActionEvent;
+import com.language.App;
 
-/**
- * The Welcome page 
- * Options to sign up, login, and navigate to other features
- */
 public class HomeController {
 
     @FXML
-    private void onLoginClicked(ActionEvent event) throws IOException {
+    private Label label;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    private ImageView whaleImage;
+
+    public void initialize() {
+        // Load the whale image
+        Image whale = new Image(App.class.getResourceAsStream("/com/language/images/whale.png"));
+        whaleImage.setImage(whale);
+    }
+
+    @FXML
+    private void onLoginClicked(ActionEvent event) throws Exception {
         App.setRoot("login");
     }
 
     @FXML
-    private void onSignupClicked(ActionEvent event) throws IOException {
+    private void onSignupClicked(ActionEvent event) throws Exception {
         App.setRoot("signup");
     }
-
 }
