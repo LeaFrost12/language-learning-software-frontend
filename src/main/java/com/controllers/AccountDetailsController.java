8f3914357d527b7b4fdc68980447ce8a45fefc89
sofiabacha <sofbacha01@gmail.com>
2024-12-05 13:44:20 -0500

    accountDetails fxml and controller

---
+++ src/main/java/com/controllers/AccountDetailsController.java
@@ -0,0 +1,30 @@
+package com.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class AccountDetailsController {
+
+    @FXML
+    private TextField txt_email;
+
+    @FXML
+    private TextField txt_first_name;
+
+    @FXML
+    private TextField txt_last_name;
+
+    @FXML
+    private TextField txt_password;
+
+    @FXML
+    private TextField txt_phone_number;
+
+    @FXML
+    private TextField txt_username;
+
+    @FXML
+    private ImageView whaleImage;
+
+}
