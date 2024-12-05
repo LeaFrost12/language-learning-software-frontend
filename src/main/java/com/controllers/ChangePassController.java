9d486cdfd8667e2fa9078fc643554ee835705694
sofiabacha <sofbacha01@gmail.com>
2024-12-05 13:34:43 -0500

    ChangePass fxml and Controller

---
+++ src/main/java/com/controllers/ChangePassController.java
@@ -0,0 +1,57 @@
+package com.controllers;
+
+import java.io.IOException;
+import com.language.App;
+import com.model.User;
+import com.model.UserList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+
+/**
+ * Controller for the Login page
+ */
+public class ChangePassController {
+
+
+    @FXML
+    private TextField txt_username;
+    @FXML
+    private TextField txt_oldPassword;
+    @FXML
+    private TextField txt_newPassword;
+    @FXML
+    private TextField txt_confirmPassword;
+    @FXML
+    private Label lbl_error;
+
+    private UserList userList;
+
+    public ChangePassController() {
+        // Initialize the UserList singleton
+        this.userList = UserList.getInstance();
+    }
+    @FXML
+    private void btnLoginClicked(ActionEvent event) throws IOException {
+        String oldPassword = txt_oldPassword.getText();
+        String newPassword = txt_newPassword.getText();
+
+        if (userList.validPass(username, password)) {
+            // Set the current user
+            User loggedInUser = userList.getUser(username);
+            userList.setCurrentUser(loggedInUser);
+
+            // Navigate to the user home page
+            App.setRoot("user_home");
+        } else {
+            lbl_error.setText("Passwords did not match. Please try again.");
+        }
+    }
+
+    @FXML
+    private void back(ActionEvent event) throws IOException {
+        App.setRoot("settings");
+    }
+
+}
