ba00506ee5c3ad6e03149231679734850ab19ba0
LeaFrost12 <leafrost012@gmail.com>
2024-12-05 13:31:43 -0500

    Adding a back button to badges

--- src/main/java/com/controllers/BadgeController.java
+++ src/main/java/com/controllers/BadgeController.java
@@ -1,10 +1,24 @@
 package com.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import com.model.Badge;
 import com.model.BadgeList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import com.language.App;
+import java.io.IOException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class BadgeController {
 
@@ -20,5 +34,11 @@ public class BadgeController {
             Text badgeText = new Text("Badge: " + badge.getName() + "\nDescription: " + badge.getDescription());
             badgeContainer.getChildren().add(badgeText);
         }
+
+    }
+
+    @FXML
+    private void back(MouseEvent event) throws IOException {
+        App.setRoot("user_home");
     }
 }
