1091b2bd99d4025944d5831b35075f308e533867
mm249 <mm249@email.sc.edu>
2024-12-05 13:51:26 -0500

    creating language option page

--- src/main/java/com/controllers/LanguageController.java
+++ src/main/java/com/controllers/LanguageController.java
@@ -1,5 +1,48 @@
 package com.controllers;
 
+import com.language.App;
+import com.model.LanguageSystemFacade;
+import com.model.LanguagesEnum;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class LanguageController {
-    
+
+    @FXML
+    private VBox languageButtonsContainer;
+
+    private final LanguageSystemFacade facade = LanguageSystemFacade.getInstance();
+
+    public void initialize() {
+        // Dynamically create buttons for each language in LanguagesEnum
+        for (LanguagesEnum language : LanguagesEnum.values()) {
+            Button languageButton = new Button(language.name());
+            languageButton.setStyle("-fx-font-size: 14pt; -fx-padding: 10px;");
+            languageButton.setOnAction(e -> selectLanguage(language));
+            languageButtonsContainer.getChildren().add(languageButton);
+        }
+    }
+
+    private void selectLanguage(LanguagesEnum language) {
+        boolean success = facade.loadLanguage(language.name());
+        if (success) {
+            System.out.println("Language selected: " + language.name());
+            // Navigate to user home or next appropriate page
+            App.setRoot("user_home");
+        } else {
+            System.out.println("Failed to load language: " + language.name());
+        }
+    }
+
+    @FXML
+    private void onBackHomeClicked(ActionEvent event) throws IOException {
+        // Navigate back to the User Home page
+        App.setRoot("user_home");
+    }
 }
+
