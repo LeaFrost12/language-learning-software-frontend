c02de2ce5b4fbac8efeaccda2dd40d6870f23d27
mm249 <mm249@email.sc.edu>
2024-12-06 23:18:24 -0500

    badges

--- src/main/java/com/controllers/BadgeController.java
+++ src/main/java/com/controllers/BadgeController.java
@@ -4,32 +4,47 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import com.model.Badge;
-import com.model.BadgeList;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import com.language.App;
 import java.io.IOException;
-
 
 public class BadgeController {
 
     @FXML
     private VBox badgeContainer;
 
+    @FXML
+    private Button backButton;
+
+    // Flag to determine if the badge has already been added
+    private boolean badgeAdded = false;
+
     public void initialize() {
-        // Get the instance of BadgeList and display badges
-        BadgeList badgeList = BadgeList.getInstance();
-
-        for (Badge badge : badgeList.getBadges()) {
-            // Create a Text node for each badge
-            Text badgeText = new Text("Badge: " + badge.getName() + "\nDescription: " + badge.getDescription());
-            badgeContainer.getChildren().add(badgeText);
+        // Dynamically add badges to the container
+        if (!badgeAdded) {
+            addBadgeImage();
+            badgeAdded = true;
         }
-
     }
 
-    /**
-     * Goes back to the user home page
-     */
+    private void addBadgeImage() {
+        // Create an ImageView for the badge
+        ImageView badgeImageView = new ImageView();
+        badgeImageView.setImage(new Image(App.class.getResourceAsStream("/com/language/images/badge1.png")));
+        badgeImageView.setFitWidth(100); // Set the image width
+        badgeImageView.setFitHeight(100); // Set the image height
+        badgeImageView.setPreserveRatio(true);
+
+        // Create a description for the badge
+        Text badgeDescription = new Text("Congratulations! You earned the Lesson 1 badge!");
+        badgeDescription.setStyle("-fx-font-size: 14; -fx-padding: 5;");
+
+        // Add the badge image and description to the container
+        badgeContainer.getChildren().addAll(badgeImageView, badgeDescription);
+    }
+
     @FXML
     private void back(MouseEvent event) throws IOException {
         App.setRoot("user_home");
